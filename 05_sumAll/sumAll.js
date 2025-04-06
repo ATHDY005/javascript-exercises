a87f15c5652771f451ebd73e76bab1ddea053c49
Harini Shamlin <84709944+ATHDY005@users.noreply.github.com>
2025-04-06 06:23:24 +0000

    Fix loops and array Destructuring

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
 
+  if (
+    typeof a !== 'number' ||
+    typeof b !== 'number' ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(b)
+  )
+    return 'ERROR';
+  if (a < 0 || b < 0) return 'ERROR';
+
+  let start = Math.min(a, b);
+  let end = Math.max(a, b);
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
