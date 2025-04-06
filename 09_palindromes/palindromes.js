414bc9c9961f54c61d1929e082b8aeb19df37b88
Harini Shamlin <84709944+ATHDY005@users.noreply.github.com>
2025-04-06 08:56:40 +0000

    Fix Fibonacci Logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const reverseString = require('../03_reverseString/reverseString');
 
+const palindromes = function (string) {
+  const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((elements) => alphanumeric.includes(elements))
+    .join('');
+  let reversed = cleanedString.split('').reverse().join('');
+  return cleanedString === reversed;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+  let oldest = 0;
+  let old = 1;
+  let newInput = Number(input);
+  let fibbo;
+  if (newInput === 0) return 0;
+  if (newInput === 1) return 1;
+  if (newInput < 0) return 'OOPS';
+  for (let i = 2; i <= newInput; i++) {
+    fibbo = oldest + old;
+    oldest = old;
+    old = fibbo;
+  }
+  return fibbo;
 };
 
 // Do not edit below this line
